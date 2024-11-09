ae8de0c3c396ace6a0a42769ae13c764423520db
Tuan Minh <ming18380@gmail.com>
2024-11-09 10:17:23 +0700

    commit portfolio

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import Logo from "../assets/kevinRushLogo.png"
+import  {FaLinkedin} from "react-icons/fa"
+import  {FaGithub } from "react-icons/fa"
+import  { FaFacebook } from "react-icons/fa"
+import  { FaInstagram } from "react-icons/fa"
+function Navbar() {
+  return (
+    <nav className=" mb-20 flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
+        <img className="mx-2 w-10 " src={Logo} alt="Logo" />
+      </div>
+      <div className="m-8 flex items-center justify-between gap-4 text-2xl">
+        <FaLinkedin/>
+        <FaGithub/>
+        <FaFacebook/>
+        <FaInstagram/>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ src/constants/index.js
@@ -0,0 +1,80 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `Tôi là lập trình viên đam mê với tiền và sở trường làm trang web lớn và có khả năng mở rộng. Với 1 năm kinh nghiệmt thực, tôi đã có được trong mình những skill Front-end tech như React, Next.js, Remix, cũng như là công nghệ backend bao gồm Nodejs, Asp.Net, MySQL, SQLserver, Mongodb. Mục đích hướng đến của tôi là kiếm ra tiền và đóng góp thật nhiều trong các dự án dẫn đầu trong công nghệ và phát triển kĩ năng.`;
+
+export const ABOUT_TEXT = `Là một lập trình viên với hơn một năm kinh nghiệm thực chiến, tôi đã xây dựng nền tảng vững chắc về lập trình và công nghệ, không ngừng học hỏi và phát triển các kỹ năng của mình. Dù kinh nghiệm còn khiêm tốn, tôi đã nhanh chóng tiếp thu và nắm vững các kiến thức nâng cao, từ phát triển web, backend đến kỹ thuật tối ưu hiệu suất.
+
+Với niềm đam mê rõ rệt trong việc khai thác công nghệ để tối ưu hóa lợi nhuận và cơ hội kiếm tiền, tôi không ngừng tìm kiếm những phương pháp sáng tạo và giải pháp công nghệ mang lại giá trị thực tiễn. Đối với tôi, lập trình không chỉ là viết mã mà còn là công cụ để biến ý tưởng thành sản phẩm, tối ưu hóa quy trình và nâng cao hiệu quả kinh doanh.
+
+Luôn hướng đến hiệu quả, nhanh nhạy với xu hướng và kiên định trong việc phát triển bản thân, tôi sẵn sàng đón nhận thử thách mới và hợp tác để tạo ra các giải pháp công nghệ bền vững và mang lại lợi nhuận.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2023 - Present",
+    role: "Senior Full Stack Developer",
+    company: "Google Inc.",
+    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
+    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+  },
+  {
+    year: "2022 - 2023",
+    role: "Frontend Developer",
+    company: "Adobe",
+    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
+    technologies: ["HTML", "CSS", "React.js", "mySQL"],
+  },
+  {
+    year: "2021 - 2022",
+    role: "Full Stack Developer",
+    company: "Facebook",
+    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
+    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
+  },
+  {
+    year: "2020 - 2021",
+    role: "Software Engineer",
+    company: "Paypal",
+    description: `Contributed to the development of web applications using JavaScript, Typescript, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
+    technologies: ["Express", "React", "Typescript", "Node.js"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "E-Commerce Website",
+    image: project1,
+    description:
+      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+  },
+  {
+    title: "Task Management App",
+    image: project2,
+    description:
+      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
+    technologies: ["HTML", "CSS", "Remix", "Firebase"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React.js", "Tailwind"],
+  },
+  {
+    title: "Blogging Platform",
+    image: project4,
+    description:
+      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+    technologies: ["HTML", "CSS", "Next.js", "Express", "mySQL"],
+  },
+];
+
+export const CONTACT = {
+  address: "405/8, Trường Chinh, phường 14, quận Tân Bình, TPHCM ",
+  phoneNo: "+84 38773126 ",
+  email: "ming18380@gmail.com",
+};
